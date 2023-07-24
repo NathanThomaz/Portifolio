98262c2f36f3793678f75a79dde405a83cf8c296
Nathan Thomaz <nathanthomaz@gmail.com>
2023-07-24 11:24:25 -0300

    criando o js

--- scrypt.js
+++ scrypt.js
@@ -1,1 +1,13 @@
+function ativaLetra(elemento){
+    const arrayTexto = elemento.innerHTML.split('')
+    elemento.innerHTML = '';
+    arrayTexto.array.forEach(( letra ,i ) => {
+        setTimeout(()=>{
+            elemento.innerHTML += letra;
+        }, 75 * i);
+    });
+}
 
+ativaLetra(titulo);
+const titulo = document.querySelector('#sub_inf');
+console.log(titulo);
